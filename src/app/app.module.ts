257533932e127fcaf9dc5678f2cbdcb14c53c158
mzeidler <mzeidler3@gmail.com>
2019-04-15 18:57:06 +0200

    Angular Material Modules import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import {MatNativeDateModule } from '@angular/material';
+import { MatNativeDateModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [
@@ -33,7 +36,10 @@ import {MatNativeDateModule } from '@angular/material';
     MatRadioModule,
     AppRoutingModule,
     MatDatepickerModule,
-    MatNativeDateModule ,
+    MatNativeDateModule,
+    MatExpansionModule,
+    MatChipsModule,
+    MatCardModule,
     MatInputModule,
     HttpClientModule
   ],
