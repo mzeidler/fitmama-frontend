a882bc72faed9450783da60cad5dc6a954d54994
mzeidler <mzeidler3@gmail.com>
2019-04-19 20:24:58 +0200

    Loading users in MenusComponent

--- src/app/fitMenusModule/menus/menus.component.ts
+++ src/app/fitMenusModule/menus/menus.component.ts
@@ -3,6 +3,8 @@ import { MenusService } from 'src/app/services/menus.service';
 import { Menu } from 'src/app/model/menu';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { AddMenuDialogComponent } from '../add-menu-dialog/add-menu-dialog.component';
+import { User } from 'src/app/model/user';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-menus',
@@ -12,15 +14,17 @@ import { AddMenuDialogComponent } from '../add-menu-dialog/add-menu-dialog.compo
 export class MenusComponent implements OnInit {
 
   menus: Menu[];
+  users: User[];
   name: string;
 
-  constructor(private menusService: MenusService, public dialog: MatDialog) { }
+  constructor(private menusService: MenusService, private userService: UsersService, public dialog: MatDialog) { }
 
   ngOnInit() {
   }
 
   getMenus(): void {
     this.menusService.getMenus().subscribe(menus => this.menus = menus);
+    this.userService.getUsersShort().subscribe(users => this.users = users.users); 
   }
 
   addMenu(): void {
