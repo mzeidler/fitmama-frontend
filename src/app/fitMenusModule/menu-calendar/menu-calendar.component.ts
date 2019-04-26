5a222644dac16435290b12749bf54a9032167d7f
mzeidler <mzeidler3@gmail.com>
2019-04-26 19:49:25 +0200

    MenuCalendar: customCellTemplate

--- src/app/fitMenusModule/menu-calendar/menu-calendar.component.ts
+++ src/app/fitMenusModule/menu-calendar/menu-calendar.component.ts
@@ -3,6 +3,7 @@ import { Menu } from 'src/app/model/menu';
 import { CalendarEvent, CalendarEventTimesChangedEvent } from 'angular-calendar';
 import { mycolors } from '../../const/mycolors';
 import { Subject } from 'rxjs';
+import { MonthViewDay } from 'calendar-utils';
 
 @Component({
   selector: 'app-menu-calendar',
@@ -31,6 +32,7 @@ export class MenuCalendarComponent implements OnInit {
       ev.title = menuDay.name;
       ev.color = mycolors.yellow;
       ev.start = new Date(menuDay.day);
+      ev.id = menuDay.id;
       this.events.push(ev);
     });
   }
@@ -45,5 +47,27 @@ export class MenuCalendarComponent implements OnInit {
 
   closeOpenMonthViewDay() {
     this.activeDayIsOpen = false;
+  }
+
+  doubleClick(day: MonthViewDay) {
+    let ev: CalendarEvent = this.findEvent(day);
+    if (ev) {
+      console.log("doubleclick event: , id=" + ev.id + ", title=" + ev.title);
+    } else {
+      console.log("Add new event at " + day);
+    }    
+  }
+
+  findEvent(day: MonthViewDay): CalendarEvent {
+    let currentEvent: CalendarEvent = undefined;
+    this.events.forEach(event => {
+      if (event.start.getFullYear() == day.date.getFullYear() && 
+          event.start.getMonth() == day.date.getMonth() && 
+          event.start.getDate() == day.date.getDate()) {
+            currentEvent = event;
+      }      
+    });
+
+    return currentEvent;
   }
 }
