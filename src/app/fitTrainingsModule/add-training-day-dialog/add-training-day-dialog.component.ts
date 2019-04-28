d676b4066d42f7c1a4258b76461a74827c863741
mzeidler <mzeidler3@gmail.com>
2019-04-28 19:33:54 +0200

    AddTrainingDayDialogComponent

--- src/app/fitTrainingsModule/add-training-day-dialog/add-training-day-dialog.component.ts
+++ src/app/fitTrainingsModule/add-training-day-dialog/add-training-day-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import * as DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 
 @Component({
   selector: 'app-add-training-day-dialog',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTrainingDayDialogComponent implements OnInit {
 
-  constructor() { }
+  public Editor = DecoupledEditor;
+
+  constructor(public dialogRef: MatDialogRef<AddTrainingDayDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
   }
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  public onReady(editor) {
+
+    editor.editing.view.change( writer => {
+      writer.setAttribute( 'spellcheck', 'false', editor.editing.view.document.getRoot() );
+    } );
+
+    editor.ui.getEditableElement().parentElement.insertBefore(
+        editor.ui.view.toolbar.element,
+        editor.ui.getEditableElement()
+    );
+  }
 }
