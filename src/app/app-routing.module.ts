3a607f3af9964b617c8a27f0ae27b312f2dd9333
mzeidler <mzeidler3@gmail.com>
2019-04-28 19:25:11 +0200

    /trainings link rename

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UsersComponent } from './fitUsersModule/users/users.component';
-import { GroupsComponent } from './fitAdminModule/groups/groups.component';
-import { UserDetailsComponent } from './fitUsersModule/user-details/user-details.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
