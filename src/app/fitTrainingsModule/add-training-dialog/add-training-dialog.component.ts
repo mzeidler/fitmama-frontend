b52450841ef71b405c7e486c6915280347fdad4b
mzeidler <mzeidler3@gmail.com>
2019-04-28 19:36:04 +0200

    AddTrainingDialogComponent

--- src/app/fitTrainingsModule/add-training-dialog/add-training-dialog.component.ts
+++ src/app/fitTrainingsModule/add-training-dialog/add-training-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-add-training-dialog',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTrainingDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<AddTrainingDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
   }
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
