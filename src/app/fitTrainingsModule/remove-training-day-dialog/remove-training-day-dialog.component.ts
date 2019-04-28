706f132a58c8bf3c008f7d2c72ccc09daf8b0c82
mzeidler <mzeidler3@gmail.com>
2019-04-28 19:44:09 +0200

    RemoveTrainingDayDialogComponent

--- src/app/fitTrainingsModule/remove-training-day-dialog/remove-training-day-dialog.component.ts
+++ src/app/fitTrainingsModule/remove-training-day-dialog/remove-training-day-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-remove-training-day-dialog',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RemoveTrainingDayDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<RemoveTrainingDayDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
   }
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
