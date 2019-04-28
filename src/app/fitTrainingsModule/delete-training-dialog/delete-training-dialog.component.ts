018a7c1c759113d185a896c8d099f58a8c62d7a0
mzeidler <mzeidler3@gmail.com>
2019-04-28 19:38:31 +0200

    DeleteTrainingDialogComponent

--- src/app/fitTrainingsModule/delete-training-dialog/delete-training-dialog.component.ts
+++ src/app/fitTrainingsModule/delete-training-dialog/delete-training-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-delete-training-dialog',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteTrainingDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<DeleteTrainingDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
   }
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
