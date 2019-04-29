48a31557af430fc174c5da1059c31ffd9fc13485
mzeidler <mzeidler3@gmail.com>
2019-04-29 21:26:45 +0200

    menus.ts, roles.ts, trainings.ts

---
+++ src/app/model/menus.ts
@@ -0,0 +1,5 @@
+import { Menu } from './menu';
+
+export class Menus {
+    menus: Menu[];
+}
---
+++ src/app/model/trainings.ts
@@ -0,0 +1,5 @@
+import { Training } from './training';
+
+export class Trainings {
+    trainings: Training[];
+}
