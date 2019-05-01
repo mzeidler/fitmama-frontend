bdfeadfd2e71af523a1e154550740722e28ec9a6
mzeidler <mzeidler3@gmail.com>
2019-05-01 20:59:51 +0200

    user details component

--- src/app/fitUsersModule/user-details/user-details.component.ts
+++ src/app/fitUsersModule/user-details/user-details.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-import { UsersService } from '../../services/users.service';
 import { User } from '../../model/user';
 
 @Component({
@@ -11,27 +8,11 @@ import { User } from '../../model/user';
 })
 export class UserDetailsComponent implements OnInit {
 
-  @Input()
-  user: User;
-
-  constructor(
-    private route: ActivatedRoute,
-    private usersService: UsersService,
-    private location: Location
-  ) {}
+  constructor() {}
 
   ngOnInit() {
-    this.getUser();
   }
 
-  getUser(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.usersService.getUser(id)
-      .subscribe(user => this.user = user);
-  }
 
-  goBack(): void {
-    this.location.back();
-  }
 
 }
