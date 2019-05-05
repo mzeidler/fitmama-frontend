f33a3baff247671a9eb94b878b99e7f9dbce1f09
mzeidler <mzeidler3@gmail.com>
2019-05-05 22:28:18 +0200

    app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './services/authentication.service';
+import { User } from './model/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Fit Mama';
+  currentUser: User;
+
+  constructor(
+      private router: Router,
+      private authenticationService: AuthenticationService) {
+      this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  get isAdmin() {
+    let isAdmin = false;
+    if (this.currentUser && this.currentUser.roles) {
+      this.currentUser.roles.forEach(role => {
+        if (role.role == 'ADMIN') {
+          isAdmin = true;
+        }
+      });
+    }
+    return isAdmin;
+  }
+
+  get isUser() {
+    let isUser = false;
+    if (this.currentUser && this.currentUser.roles) {
+      this.currentUser.roles.forEach(role => {
+        if (role.role == 'USER') {
+          isUser = true;
+        }
+      });
+    }
+    return isUser;
+  }
+
+  get isExerciseAdmin() {
+    let isExerciseAdmin = false;
+    if (this.currentUser && this.currentUser.roles) {
+      this.currentUser.roles.forEach(role => {
+        if (role.role == 'EXERCISE_ADMIN') {
+          isExerciseAdmin = true;
+        }
+      });
+    }
+    return isExerciseAdmin;
+  }
+
+  get isMenuAdmin() {
+    let isMenuAdmin = false;
+    if (this.currentUser && this.currentUser.roles) {
+      this.currentUser.roles.forEach(role => {
+        if (role.role == 'MENU_ADMIN') {
+          isMenuAdmin = true;
+        }
+      });
+    }
+    return isMenuAdmin;
+  }
+
+  logout() {
+      this.authenticationService.logout();
+      this.router.navigate(['/login']);
+  }
 }
