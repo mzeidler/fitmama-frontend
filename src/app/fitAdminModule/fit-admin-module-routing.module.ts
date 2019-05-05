ffe829e0d0383fa7101583ed1292e3695eb25ba5
mzeidler <mzeidler3@gmail.com>
2019-05-05 21:36:18 +0200

    FitAdmin Module Routing

--- src/app/fitAdminModule/fit-admin-module-routing.module.ts
+++ src/app/fitAdminModule/fit-admin-module-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'admin', component: AdminComponent }
+  { 
+    path: 'admin', 
+    component: AdminComponent, 
+    canActivate: [AuthGuard], 
+    data: { roles: ['ADMIN'] } 
+  }
 ];
 
 @NgModule({
